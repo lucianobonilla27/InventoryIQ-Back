4746fb300019f7fa01bb108f32c6ad9f8d83b305
Luciano <lucianobonilla27@gmail.com>
2024-04-03 12:36:45 -0300

    agregar producto funcionando

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -10,6 +10,29 @@ const getAllProducts = async (req, res) => {
     }
 }
 
+//Agregar producto
+const agregarProducto = async (req, res) => {
+    try {
+        const { nombre, descripcion, imagenUrl, cantidad, categoria, fecha } = req.body;
+        const nuevoProducto = new ProductoModel({
+            nombre,
+            descripcion,
+            imagenUrl,
+            cantidad,
+            categoria,
+            fecha
+        });
+        await nuevoProducto.save();
+        res.status(201).json({ message: 'Producto agregado correctamente' });
+
+    } catch (error) {
+        res.status(400).json({ message: 'Error al agregar el producto' });
+        console.log(error)
+
+    }
+}
+
 export default {
-    getAllProducts
+    getAllProducts,
+    agregarProducto
 }
--- router/productsRouter.js
+++ router/productsRouter.js
@@ -7,4 +7,7 @@ const router = express.Router();
 //Traer los productos
 router.get('/products', productsController.getAllProducts)
 
+//Agregar un producto
+router.post('/agregarProduct', productsController.agregarProducto)
+
 export default router
